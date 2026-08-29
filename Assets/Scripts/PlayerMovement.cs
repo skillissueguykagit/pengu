a05a069226210de6724eb6ff626b09b0273b0430
mayank <270887602+skillissueguykagit@users.noreply.github.com>
2026-08-29 11:32:15 +0530

    added jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float moveSpeed = 4f;
-    public Rigidbody2D rb;
+    [SerializeField] private float moveSpeed = 4f;
+    [SerializeField] private float jumpForce = 5;
+    private Rigidbody2D rb;
     private float xInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 }
